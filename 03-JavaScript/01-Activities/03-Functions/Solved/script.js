0777d123a30e10176bb4141c972e1bcf0cb6ecca
Matt Kim <matthewjkim@gmail.com>
2021-11-09 21:00:54 -0500

    week 4 module

--- 03-JavaScript/01-Activities/03-Functions/Solved/script.js
+++ 03-JavaScript/01-Activities/03-Functions/Solved/script.js
@@ -1,32 +1,65 @@
-// isEqual using function declaration
-function isEqual(x, y) {
-  if (x === y) {
+
+function checkEquality(value1, value2) {
+  if (value1 === value2) {
     console.log('They are equal in type and value');
-  } else if (x == y) {
+  } else if (value1 == value2) { //non strict equality
     console.log('They are equal in value');
   } else {
     console.log('They are not equal');
   }
-  return;
 }
 
-// Logs "They are equal in type and value"
-isEqual(10, 10);
+checkEquality(10, "10");
 
-// Logs "They are equal in value"
-isEqual('10', 10);
 
-// Using function expression
-var isEqualTakeTwo = function(x, y) {
-  if (x === y) {
-    console.log('They are equal in type and value');
-  } else if (x == y) {
-    console.log('They are equal in value');
-  } else {
-    console.log('They are not equal');
-  }
-  return;
-};
 
-// Logs "They are not equal"
-isEqualTakeTwo(10, true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // isEqual using function declaration
+// function isEqual(x, y) {
+//   if (x === y) {
+//     console.log('They are equal in type and value');
+//   } else if (x == y) {
+//     console.log('They are equal in value');
+//   } else {
+//     console.log('They are not equal');
+//   }
+//   return;
+// }
+
+// // Logs "They are equal in type and value"
+// isEqual(10, 10);
+
+// // Logs "They are equal in value"
+// isEqual('10', 10);
+
+// // Using function expression
+// var isEqualTakeTwo = function(x, y) {
+//   if (x === y) {
+//     console.log('They are equal in type and value');
+//   } else if (x == y) {
+//     console.log('They are equal in value');
+//   } else {
+//     console.log('They are not equal');
+//   }
+//   return;
+// };
+
+// // Logs "They are not equal"
+// isEqualTakeTwo(10, true);
--- 03-JavaScript/01-Activities/05-Object-This/Unsolved/script.js
+++ 03-JavaScript/01-Activities/05-Object-This/Unsolved/script.js
@@ -1,18 +1,22 @@
-// TODO: What is `this` referring to here?
-console.log(this);
+
 
 // TODO: What is `this` referring to here?
 function helloThis() {
   console.log('Inside this function, this is ' + this);
 }
 
+helloThis(); //globally scoped function
+
 // TODO: What is `this` referring to here? What will be logged in the console?
 var child = {
-  age: 10,
-  ageTenYears: function() {
+  age: 10, // Number
+  ageTenYears: function() { //
     console.log(this.age + 10);
   }
 };
+
+child.ageTenYears();
+
 
 // TODO: What is `this` referring to here? What will be logged in the console?
 var investor = {
@@ -20,9 +24,12 @@ var investor = {
   investment: {
     initialInvestment: 5000,
     investmentGrowth: function() {
+      console.log(this);
       console.log(this.initialInvestment * 1.15);
     }
   }
 };
+
+investor.investment.investmentGrowth();
 
 // TODO: Call the `helloThis` function and the object methods to check results in the console
